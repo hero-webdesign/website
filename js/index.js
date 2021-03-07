0e378535178f63dd5b3c8abc16bc136d77eb60ab
Robert Dams <I400338@fhict.nl>
2021-03-07 13:24:13 +0100

    Added new section and footer to index.html

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(window).on('scroll', function() {
-    if ($(".content-image.image-right").is(':visible')) {
-        $(".content-image.image-right").addClass("loaded");
-    }
-    if ($(".content-image.image-left").is(':visible')) {
-        $(".content-image.image-left").addClass("loaded");
-    }
-});
